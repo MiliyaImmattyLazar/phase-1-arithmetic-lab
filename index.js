1993a7a061322efc68787a1e247d1498368ca2c9
Miliya Immatty Lazar <miliyaninu@gmail.com>
2024-09-20 14:30:43 +1000

    completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1= 31
+const num2 =2
+
+const multiply = num1 * num2;
+const random = 5
+const num3= 9
+const num4 =31
+const mod= num4 % num3
+
+let numbers = [10, 5, 20, 3, 15];
+
+
+let max = Math.max(...numbers);
+
+console.log(max); 
